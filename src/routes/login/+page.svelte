6a2dd0369d9408276c11fdb4db21336755868fe9
Gus Ruben <gusruben@protonmail.com>
2023-05-11 14:54:05 -0400

    Move login to component + /login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Login from './Login.svelte';
+</script>
+
+<section class="hero">
+	<div class="hero-body">
+		<h1 class="title is-1">Aspire</h1>
+	</div>
+</section>
+
+<section class="section has-text-centered">
+	<Login />
+</section>
---
+++ src/routes/login/Login.svelte
@@ -0,0 +1,41 @@
+<form action="/login" id="login" class="has-text-centered">
+    <h1 class="title">Log In</h1>
+    <div class="field">
+		<label for="" class="label">Username</label>
+
+		<div class="control has-icons-left">
+			<input type="text" class="input" placeholder="Aspen username/email" />
+			<span class="icon is-small is-left">
+				<i class="fas fa-user" />
+			</span>
+		</div>
+	</div>
+
+	<div class="field">
+		<label for="" class="label">Password</label>
+		<div class="control has-icons-left">
+			<input type="password" class="input" placeholder="Aspen password" />
+			<span class="icon is-small is-left">
+				<i class="fas fa-key" />
+			</span>
+		</div>
+	</div>
+
+	<div class="field">
+		<div class="control">
+			<button class="button is-primary">
+				<span class="icon is-small">
+					<i class="fas fa-right-to-bracket" />
+				</span>
+				<span>Log In</span>
+			</button>
+		</div>
+	</div>
+</form>
+
+<style lang="scss">
+    #login {
+        max-width: 40rem;
+        margin: auto;
+    }
+</style>
