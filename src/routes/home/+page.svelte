aebdd6055362aca182c145c4600eabbf2410d8ba
Gus Ruben <gusruben@protonmail.com>
2023-05-24 12:12:19 -0400

    Set up sidebar

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -4,6 +4,40 @@
 	import Navbar from "./Navbar.svelte";
 </script>
 
-<Calendar />
 <Navbar />
-<ClassTable />
+<div class="layout">
+	<div class="sidebar">
+		<div class="calendar">
+			<Calendar />
+		</div>
+	</div>
+	<div class="main">
+		<ClassTable />
+	</div>
+</div>
+
+
+<style lang="scss">
+	@import "bulma/sass/utilities/mixins";
+
+	.layout {
+		//  if the screen is too small for the table to be AT LEAST <$table_min_width>,
+		//  stack everything
+		@include from($sidebar-width + $table-min-width) {
+			display: flex;
+		}
+		width: 100%;
+
+		> * {
+			flex-grow: 1;
+		}
+
+		.sidebar {
+			max-width: $sidebar-width;
+
+			> .calendar {
+				margin: 15px;
+			}
+		}
+	}
+</style>
