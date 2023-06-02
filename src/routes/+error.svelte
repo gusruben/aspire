62689a7d1b1f9056a5bc579533d1456973a5b129
Gus Ruben <gusruben@protonmail.com>
2023-06-02 14:13:19 -0400

    Add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Error404 from './Error404.svelte';
+</script>
+
+<div class="main">
+    <Error404 />
+</div>
+
+<style lang="scss">
+    .main {
+        height: 100vh;
+    }
+</style>
---
+++ src/routes/Error404.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    let loggedIn = false;
+
+    onMount(() => {
+        loggedIn = !!document.cookie.includes("session=");
+    })
+</script>
+
+<div class="wrapper has-text-centered">
+    <h1 class="big-text">404</h1>
+    <h3 class="title is-3">Sorry, that page doesn't exist.</h3>
+    {#if loggedIn}
+        <a href="/home">Take me home!</a>
+    {:else}
+        <a href="/">Take me home!</a>
+    {/if}
+</div>
+
+<style lang="scss">
+    @use "sass:color";
+
+    .wrapper {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        .big-text {
+            font-size: 12rem;
+            font-weight: bold;
+        }
+    }
+</style>
