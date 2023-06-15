1a8f96cf807c5aa1955dd2f545c3179fc5d43daa
Gus Ruben <gusruben@protonmail.com>
2023-06-15 09:31:59 -0400

    Add class pages w/ data loading

---
+++ src/routes/class/[slug]/+page.server.ts
@@ -0,0 +1,26 @@
+import { redirect } from '@sveltejs/kit';
+import { getUser } from '../../../lib/users';
+// for some reason, this only works with the full path
+import { AspenApiError } from '../../../../node_modules/aspen-api/dist/types.js';
+
+export async function load({ cookies, params }) {
+	const user = getUser(cookies.get("session") as string);
+    
+    if (!user) {
+        cookies.delete("session");
+        throw redirect(302, "/login");
+    }
+
+    try {
+        return {
+            class: await user.getClass(params.slug),
+            assignments: await user.getAssignments(params.slug),
+        }
+    } catch (e) {
+        // this will make it return the unknown class page, rather than an error
+        if ((e as Error).message == AspenApiError.UnknownClassError) {
+            return {};
+        }
+        throw e;
+    }
+}
